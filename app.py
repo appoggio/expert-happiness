971f1aa293b7f445849c0223de26f594f19128c2
appoggio <98469596+appoggio@users.noreply.github.com>
2022-01-26 16:27:07 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ import streamlit as st
 from data import *
 from input import image_input, webcam_input
 
-st.title("Neural Style Transfer by INTELLIGENZAARTIFICIALEITALIA.NET")
-html_string = "<h3>this is an html string</h3>"
+
+st.markdown("<h1 style='text-align: center; background-color: black;font-size:16px;'><bold style='color:green;'>INTELLIGENZA<bold style='color:white;'>ARTIFICIALE<bold style='color:red;'>ITALIA</bold></bold></bold></h1><br><small>Prova la nostra Intelligenza Artificiale di Neural Style Transfer", unsafe_allow_html=True)
+
+html_string = "<h3><br></h3>"
 
 st.markdown(html_string, unsafe_allow_html=True)
 
