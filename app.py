388d724350587e963d441ef5caf5e120315e74f3
appoggio <98469596+appoggio@users.noreply.github.com>
2022-01-26 16:21:50 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -4,9 +4,11 @@ from data import *
 from input import image_input, webcam_input
 
 st.title("Neural Style Transfer by INTELLIGENZAARTIFICIALEITALIA.NET")
+html_string = "<h3>this is an html string</h3>"
 
-method = st.radio("\n \n \nDa dove vuoi prendere l'immagine da modificare ? ", options=['Webcam Live', 'Voglio caricare una foto'])
+st.markdown(html_string, unsafe_allow_html=True)
 
+method = st.radio("Da dove vuoi prendere l'immagine da modificare ? ", options=['Webcam Live', 'Voglio caricare una foto'])
 
 style_model_name = st.selectbox("Scegli il modello che vuoi applicare", style_models_name)
 
