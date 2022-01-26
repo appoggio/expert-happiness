cc453d687edd4d591dd6baba937077724c7a5d9f
appoggio <98469596+appoggio@users.noreply.github.com>
2022-01-26 16:31:14 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -4,16 +4,18 @@ from data import *
 from input import image_input, webcam_input
 
 
-st.markdown("<h1 style='text-align: center; background-color: black;font-size:16px;'><bold style='color:green;'>INTELLIGENZA<bold style='color:white;'>ARTIFICIALE<bold style='color:red;'>ITALIA</bold></bold></bold></h1><br><small>Prova la nostra Intelligenza Artificiale di Neural Style Transfer", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; background-color: black;font-size:16px;'><bold style='color:green;'>INTELLIGENZA<bold style='color:white;'>ARTIFICIALE<bold style='color:red;'>ITALIA</bold></bold></bold></h1><br><center><small>Prova la nostra Intelligenza Artificiale di Neural Style Transfer", unsafe_allow_html=True)
 
-html_string = "<h3><br></h3>"
 
-st.markdown(html_string, unsafe_allow_html=True)
 
 with st.beta_expander("Opzioni"):
 	method = st.radio("Da dove vuoi prendere l'immagine da modificare ? ", options=['Webcam Live', 'Voglio caricare una foto'])
 
 	style_model_name = st.selectbox("Scegli il modello che vuoi applicare", style_models_name)
+
+html_string = "<h3><br></h3>"
+
+st.markdown(html_string, unsafe_allow_html=True)
 
 if method == 'Voglio caricare una foto':
     image_input(style_model_name)
